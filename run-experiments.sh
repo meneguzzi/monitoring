36a7ee2aa1f9c69f87e4b45dd7abe2cf72f88303
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2017-11-13 18:35:30 -0200

    Minor error in script for actions.

--- run-experiments.sh
+++ run-experiments.sh
@@ -8,8 +8,11 @@ export PYTHONPATH=.
 
 STARTTIME=$(date +%s)
 
-python gp/gp.py 1 & python gp/gp.py 2 & python gp/gp.py 3 & python gp/gp.py 4 & python gp/gp.py 5
-python gp/gp.py 6 & python gp/gp.py 7 & python gp/gp.py 8 & python gp/gp.py 9 & python gp/gp.py 10
+(python gp/gp.py 1 ; python gp/gp.py 2) &
+(python gp/gp.py 3 ; python gp/gp.py 4) &
+(python gp/gp.py 5 ; python gp/gp.py 6) &
+(python gp/gp.py 7 ; python gp/gp.py 8) &
+(python gp/gp.py 9 ; python gp/gp.py 10)
 
 ENDTIME=$(date +%s)
 
