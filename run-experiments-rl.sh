b5ca98d36d1d9c53a1a308b92c217fc3075f684c
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2020-09-29 14:57:11 -0300

    Experiments for domains BTW and Bridges

--- run-experiments-rl.sh
+++ run-experiments-rl.sh
@@ -16,16 +16,6 @@ echo "Running experiments for BD domain..."
 (python2.7 rl/rl.py bridges 3 ; python2.7 rl/rl.py bridges 4) &
 (python2.7 rl/rl.py bridges 5)
 
-echo "Running experiments for TPP domain..."
-(python2.7 rl/rl.py tpp 1 ; python2.7 rl/rl.py tpp 2) &
-(python2.7 rl/rl.py tpp 3 ; python2.7 rl/rl.py tpp 4) &
-(python2.7 rl/rl.py tpp 5)
-
-echo "Running experiments for PSR domain..."
-(python2.7 rl/rl.py psr-small 1 ; python2.7 rl/rl.py psr-small 2) &
-(python2.7 rl/rl.py psr-small 3 ; python2.7 rl/rl.py psr-small 4) &
-(python2.7 rl/rl.py psr-small 5)
-
 ENDTIME=$(date +%s)
 
 echo "Finished monitoring simulation in $(( ($ENDTIME - $STARTTIME)/60 )) minutes"
