6fe43fc8d55d9df151b576e4c97fccfeb7a86c7d
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2020-10-06 00:55:53 -0300

    Moving old results aside for new ones

--- run-experiments-rl.sh
+++ run-experiments-rl.sh
@@ -6,15 +6,23 @@ echo "### Started monitoring simulation ###"
 
 STARTTIME=$(date +%s)
 
-echo "Running experiments for BTW domain..."
+echo "Running experiments for TPP domain..."
 (python2.7 rl/rl.py tpp 1 ; python2.7 rl/rl.py tpp 2) &
-(python2.7 rl/rl.py tpp 3 ; python2.7 rl/rl.py tpp 4) &
-(python2.7 rl/rl.py tpp 5)
+(python2.7 rl/rl.py tpp 3 ; python2.7 rl/rl.py tpp 5) &
 
 echo "Running experiments for BD domain..."
+(python2.7 rl/rl.py bridges 1 ; python2.7 rl/rl.py bridges 2) &
+(python2.7 rl/rl.py bridges 3 ; python2.7 rl/rl.py bridges 4) &
+(python2.7 rl/rl.py bridges 5)
+
+echo "Running experiments for PSR domain..."
 (python2.7 rl/rl.py psr-small 1 ; python2.7 rl/rl.py psr-small 2) &
 (python2.7 rl/rl.py psr-small 3 ; python2.7 rl/rl.py psr-small 4) &
 (python2.7 rl/rl.py psr-small 5)
+
+echo "Running experiments for BTW domain..."
+(python2.7 rl/rl.py barter-world 1 ; python2.7 rl/rl.py barter-world 2) &
+(python2.7 rl/rl.py barter-world 5 ; python2.7 rl/rl.py barter-world 4) &
 
 ENDTIME=$(date +%s)
 
